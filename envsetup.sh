31ac90246022d788344a8ee17dfee46fed45a5f2
pcexpress <pcexpress805@gmail.com>
2013-10-16 15:02:45 -0700

    Linaro

--- envsetup.sh
+++ envsetup.sh
@@ -139,7 +139,7 @@ function setpaths()
     case $ARCH in
         x86) toolchaindir=x86/i686-linux-android-$targetgccversion/bin
             ;;
-        arm) toolchaindir=arm/arm-linux-androideabi-$targetgccversion/bin
+        arm) toolchaindir=arm/linaro-4.8/bin
             ;;
         mips) toolchaindir=mips/mipsel-linux-android-$targetgccversion/bin
             ;;
